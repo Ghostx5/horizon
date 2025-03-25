42fa2c89147541d1125d653660d82056ccbbef7c
Vijay <vijaymukund1217@gmail.com>
2025-03-24 21:39:43 -0500

    Initial Commit

---
+++ HorizonTests/HorizonTests.swift
@@ -0,0 +1,17 @@
+//
+//  HorizonTests.swift
+//  HorizonTests
+//
+//  Created by Vijay Venkatesan on 3/24/25.
+//
+
+import Testing
+@testable import Horizon
+
+struct HorizonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
